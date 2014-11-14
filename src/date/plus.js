70e1b507dc03d8856129296d64c72e78b85d59ca
SimpleLee <darkty2009@gmail.com>
2014-11-14 11:56:41 +0800

    add date plus function

---
+++ src/date/plus.js
@@ -0,0 +1,62 @@
+patches({
+    compare:function(date1, date2) {
+
+    },
+    equal:function(date1, date2) {
+
+    },
+    today:function() {
+
+    },
+    isLeapYear:function(year) {
+
+    }
+}, Date);
+
+patches({
+    clearTime:function() {
+
+    },
+    clone:function() {
+
+    },
+    compareTo:function(target) {
+
+    },
+    isEqual:function(target) {
+
+    },
+    isAfter:function(target) {
+
+    },
+    isBefore:function(target) {
+
+    },
+    getWeek:function() {
+
+    },
+    addSeconds:function(value) {
+
+    },
+    addMinutes:function(value) {
+
+    },
+    addHours:function(value) {
+
+    },
+    addDays:function(value) {
+
+    },
+    addWeeks:function(value) {
+
+    },
+    addMonths:function(value) {
+
+    },
+    addYears:function(value) {
+
+    },
+    add:function(config) {
+
+    }
+}, Date.prototype);
