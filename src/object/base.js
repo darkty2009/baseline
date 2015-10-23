48e0a7f5e34bc1205f2808ae8b934067bc7b4008
= <=>
2015-10-23 17:32:51 +0800

    fix detect api: patches to patch.some

--- src/object/base.js
+++ src/object/base.js
@@ -82,7 +82,7 @@
                 }
 
                 if(properties !== void 0) {
-                    patches(properties, object);
+                    patch.some(properties, object);
                 }
                 return object;
             }
