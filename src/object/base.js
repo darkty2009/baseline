46afb457d790d46fe2cd93d70239fd49cc79420f
= <=>
2016-02-02 12:44:13 +0800

    add Object.assign

--- src/object/base.js
+++ src/object/base.js
@@ -23,6 +23,25 @@
                 return v2 !== v2;
             }
             return v1 === v2;
+        },
+        assign:function(target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var output = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source !== undefined && source !== null) {
+                    for (var nextKey in source) {
+                        if (source.hasOwnProperty(nextKey)) {
+                            output[nextKey] = source[nextKey];
+                        }
+                    }
+                }
+            }
+            return output;
         }
     }, Object);
 
