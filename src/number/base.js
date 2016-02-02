9e213db057524551dc991954287f9feca74a57c7
= <=>
2016-02-02 14:28:46 +0800

    add Number.isSafeInteger

--- src/number/base.js
+++ src/number/base.js
@@ -5,11 +5,14 @@
         EPSILON:2.220446049250313e-16,
         MAX_VALUE:1.7976931348623157e+308,
         MIN_VALUE:5e-324,
-        MAX_SAFE_VALUE:9007199254740992,
-        MIN_SAFE_VALUE:-9007199254740992,
+        MAX_SAFE_VALUE:9007199254740991,
+        MIN_SAFE_VALUE:-9007199254740991,
         NEGATIVE_INFINITY:-Infinity,
         POSITIVE_INFINITY:Infinity,
         "NaN":NaN,
+        isSafeInteger:function(val) {
+            return Number.isInteger(val) && val >= Number.MIN_SAFE_VALUE && val <= Number.MAX_SAFE_VALUE;
+        },
         isFinite:function(val) {
             if(typeof val == 'number') {
                 return win.isFinite(val);
@@ -17,7 +20,7 @@
             return false;
         },
         isInteger:function isInteger (val) {
-            return typeof val === "number" && isFinite(val) && val > Number.MIN_SAFE_VALUE && val < Number.MAX_SAFE_VALUE && Math.floor(val) === val;
+            return typeof val === "number" && isFinite(val) && Math.floor(val) === val;
         },
         isNaN:isNaN,
         parseFloat:parseFloat,
