681a7212a72ba89b92df251dd464880086ed00a0
= <=>
2016-02-02 13:15:15 +0800

    add String.prototype.includes

--- src/string/base.js
+++ src/string/base.js
@@ -186,6 +186,19 @@
             }
             return true;
         },
+        includes:function() {
+            'use strict';
+            if (typeof arguments[1] === "number") {
+                if (this.length < arguments[0].length + arguments[1].length) {
+                    return false;
+                } else {
+                    if (this.substr(arguments[1], arguments[0].length) === arguments[0]) return true;
+                    else return false;
+                }
+            } else {
+                return String.prototype.indexOf.apply(this, arguments) !== -1;
+            }
+        },
         trim:function() {
             return this.trimLeft().trimRight();
         },
